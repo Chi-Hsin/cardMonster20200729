47d080bf7c2f833b3dc5ab88994a8f50a830f416
HSIN <jack1234552000@yahoo.com.tw>
2020-07-29 23:06:21 +0800

    202007292306
    先放架構上來

---
+++ js/index.js
@@ -0,0 +1,19 @@
+var indexData = new Vue({
+            el: '#outerDiv',
+            data: {
+               },
+			computed:{
+				testNumber:function(){
+				},
+			},   
+            methods:{
+            },
+			watch:{
+			},
+            created() { 
+            },
+            mounted(){
+				
+            },
+})
+		   
---
+++ js/xyz.js
@@ -0,0 +1,22 @@
+var xyzData = new Vue({
+            el: '#xyz',
+            data: {
+				xxxx:6666666
+               },
+			computed:{
+				
+			},   
+            methods:{
+				test3:function(){
+					alert("123123123132")
+				},
+            },
+			watch:{
+			},
+            created() { 
+            },
+            mounted(){
+				
+            },
+})
+		   
